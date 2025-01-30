8ade428e405265d9a594861508978d5316a47a95
rrelyk <kystik94@gmail.com>
2025-01-30 09:12:00 -0500

    All remaining player lessons.

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import pygame
 from constants import *
+from player import Player
+
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    
+    p1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
+    
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     running = True
@@ -15,10 +27,16 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         
-        screen.fill((0,0,0))
+        screen.fill("black")
+        #drawable group
+        for thing in drawable:
+            thing.draw(screen)
+        for thing in updateable:
+            thing.update(dt)
+        
         pygame.display.flip()
+        
         dt = clock.tick(60) /1000
-        print(dt)
 
 
 if __name__ == "__main__":
